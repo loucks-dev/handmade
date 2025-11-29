4c1e139d1ebf471190f7f1f298535eec1313963d
loucks-dev <parkerloucks74@gmail.com>
2025-11-28 22:33:20 -0600

    Created Windows entry point and build file

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,12 @@
+#include <windows.h> // Windows definitions
+
+
+int __clrcall WinMain(  // Windows entrypoint
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nShowCmd
+)
+{
+    return(0); 
+}
